7cd1ddf4475f707c725ab8434669265bb1b06439
Morgan2006-ui <morgan.erickson@roboken2.com>
2025-10-01 09:42:07 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,17 +7,22 @@ public class Main {
         int numberToGuess = rand.nextInt(100) + 1;
         Scanner scanner = new Scanner(System.in);
         int guess = 0;
+        int attempts = 0; // track how many guesses it took
 
         System.out.println("Guess a number between 1 and 100:");
 
         while (guess != numberToGuess) {
             System.out.print("Enter your guess: ");
             guess = scanner.nextInt();
+            attempts++;
 
-            if (guess != numberToGuess) {
-                System.out.println("Incorrect. Try again.");
+            if (guess < numberToGuess) {
+                System.out.println("Too low! Try again.")
+            } else if (guess > numberToGuess) {
+                System.out.println("Too high! Try again.");
             } else {
-                System.out.println("Correct! The number was " + numberToGuess + ".");
+            	System.out.print("Correct! The number was " to numberToGuess + ".");
+                System.out.println("You guessed it in " + attempts + " tries!");
             }
         }
 
