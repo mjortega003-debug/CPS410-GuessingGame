84341171f8ebe234b10c79f53a6cdae2f0d36d34
mjortega003-debug <mjortega003@gmail.com>
2025-10-01 10:32:49 -0400

    Add files via upload
    Initial number guesser for testing

---
+++ Main.java
@@ -0,0 +1,26 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int numberToGuess = rand.nextInt(100) + 1;
+        Scanner scanner = new Scanner(System.in);
+        int guess = 0;
+
+        System.out.println("Guess a number between 1 and 100:");
+
+        while (guess != numberToGuess) {
+            System.out.print("Enter your guess: ");
+            guess = scanner.nextInt();
+
+            if (guess != numberToGuess) {
+                System.out.println("Incorrect. Try again.");
+            } else {
+                System.out.println("Correct! The number was " + numberToGuess + ".");
+            }
+        }
+
+        scanner.close();
+    }
+}
